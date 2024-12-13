1ed86c27933358c5017cacdc397023786ae1336d
Délczeg Sándor Balázs <balazsdelczeg@gmail.com>
2024-12-13 23:15:51 +0100

    Updated pom.xml added new dependencies, Added Mapper to EventOrganizerApplication.

--- src/main/java/hu/inf/unideb/EventOrganizer/EventOrganizerApplication.java
+++ src/main/java/hu/inf/unideb/EventOrganizer/EventOrganizerApplication.java
@@ -1,14 +1,24 @@
 package hu.inf.unideb.EventOrganizer;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EventOrganizerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventOrganizerApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper(){
+		ModelMapper m = new ModelMapper();
+		return m;
+
+		//return new ModelMapper();
 	}
 
 }
