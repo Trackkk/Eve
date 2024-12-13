f553022768736f4ee59fb743c167a67246ea3847
Délczeg Sándor Balázs <balazsdelczeg@gmail.com>
2024-12-13 13:01:35 +0100

    Initial commit

---
+++ src/test/java/hu/inf/unideb/EventOrganizer/EventOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.EventOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
